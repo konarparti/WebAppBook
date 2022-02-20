e76f3077d7389e6e4f7381b2607e4c73495f1f6b
Anton <anton.gusevv.01@gmail.com>
2022-02-20 13:58:33 +0300

    add category controller and view

--- WebApp/Controllers/CategoryController.cs
+++ WebApp/Controllers/CategoryController.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+       
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Category> category = _context.Categories;
+            return View(category);
         }
     }
 }
