8f3c1d34fed59b850e15fdf3b5a5f8c895b0d008
Anton <anton.gusevv.01@gmail.com>
2022-02-20 13:31:40 +0300

    add db migration and update db

---
+++ WebApp/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
